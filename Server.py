0196957268556e73996a02b8a6e0800545f07719
rleqt <harely@campus.technion.ac.il>
2023-01-02 17:05:54 +0200

    perfect

--- Server.py
+++ Server.py
@@ -17,28 +17,27 @@ class Server:
     # def to enter
     def handle_packet(self, time_received, load_balancer):
         if self.queue.empty() and not self.is_working:
-            # TODO: if doesn't perform change to constant
-            # rate = random.exponential(scale=self.proc_rate, size=(1, 1))
+            self.total_waiting_time += Time() - time_received
             rate = random.exponential(scale=self.proc_rate)
             self.total_process_time += rate
             self.is_working = True
-            load_balancer.s.enter(rate, 1, self.switch_packet, argument=(time_received, load_balancer,))
+            load_balancer.s.enter(rate, 1, self.switch_packet, argument=(load_balancer,))
         elif self.queue.qsize() < self.max_size:
             self.queue.put(time_received)
         else:
             self.failed += 1
 
-    def switch_packet(self, time_received, load_balancer: LoadBalancer):
+    def switch_packet(self, load_balancer: LoadBalancer):
         self.packets_processed += 1
-        self.total_waiting_time += Time() - time_received
         self.last_processed = Time()
         if not self.queue.empty():
             # TODO: if doesn't perform change to constant
             rate = random.exponential(scale=self.proc_rate)
-            # rate = [[self.proc_rate]]
+            curPacketTime = self.queue.get()
+            self.total_waiting_time += Time() - curPacketTime
             self.is_working = True
             self.total_process_time += rate
-            load_balancer.s.enter(rate, 1, self.switch_packet, argument=(self.queue.get(), load_balancer,))
+            load_balancer.s.enter(rate, 1, self.switch_packet, argument=(load_balancer,))
         else:
             self.is_working = False
 
--- main.py
+++ main.py
@@ -15,17 +15,17 @@ import LoadBalancer
 
 def main():
     # total_time = int(sys.argv[1])
-    total_time = 2500  # 5000
+    total_time = 5000
     # num_of_servers = int(sys.argv[2])
-    num_of_servers = 1  # 2
+    num_of_servers = 2
     # probs = [float(sys.argv[3 + i]) for i in range(num_of_servers)]
-    probs = [1]  # [0.2, 0.8]
+    probs = [0.2, 0.8]
     # lamb = int(sys.argv[3 + num_of_servers])
-    lamb = 9  # 200
+    lamb = 200
     # queue_sizes = [int(sys.argv[4 + num_of_servers + i]) for i in range(num_of_servers)]
-    queue_sizes = [5]  # [2, 10]
+    queue_sizes = [2, 10]
     # queue_mus = [int(sys.argv[4 + 2 * num_of_servers + i]) for i in range(num_of_servers)]
-    queue_mus = [12]  # [20, 190]
+    queue_mus = [20, 190]
     load_balancer = LoadBalancer.LoadBalancer(total_time, num_of_servers, probs, lamb,
                                               queue_sizes, queue_mus)
     load_balancer.run()
